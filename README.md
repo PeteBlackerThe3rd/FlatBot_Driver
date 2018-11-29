b2e6f35463db83255f7dc20cfe775401dbef365f
Pete Blacker <P.Blacker@surrey.ac.uk>
2018-11-29 13:33:01 +0000

    Initial assembly_control and first .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# FlatBot_Driver
-Moveit Compatable ROS driver for our 2D table robot Flat Stanley
+FlatBot_Driver & support packages
+=================================
+
+This repository is dependant upon several ROS packages beyond the standard desktop-full installation. The following extra pacakges need to be installed for it to operate correctly.
+
+* ros-<version>-moveit
+* ros-<version>-ros-control
+* ros-<version>-ros-controllers
+
+Packages
+--------
+
+### Flat_Bot_Driver
+
+Moveit Compatible ROS driver for our 2D table robot Flat Stanley. This node is essentially a moveit compatible driver for any set of herkulex servos connected via UART. The current version is designed to run on a Rpi3 using the GPIO UART connection.
+
+### Assembly_Control
+
+The Assembly_Control package containing nodes to manage a set of nodes, and plan & execute pick and place operations on a real or simulated setup.
+
+### Flat_Bot_Support
+
+Moveit_Support package containing moveit & gazebo compatible URDF definition of the robot and associated 3D models
+
+### Flat_Bot_Moveit
+
+Collection of launch files and YAML files, originally created using the moveit setup assistant. These files are used to start the moveit motion planning sub-system. 
+
+### Flat_Bot_Msgs
+
+A message only package defining the types used to describe the module communication and interface of the assembly_control node
--- assembly_control/src/module_manager.cpp
+++ assembly_control/src/module_manager.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <ros/ros.h>
 #include <tf/tf.h>
+#include <urdf/model.h>
 
 class ModuleConnector
 {
@@ -18,6 +19,25 @@ class ModuleConnector
 class ModuleType
 {
 public:
+	ModuleType(std::string typeName, std::string URDFString) : name(typeName)
+	{
+		if (!urdf.initString(URDFString.c_str()))
+		{
+			ROS_ERROR("Failed to parse urdf string for module type [%s]", typeName.c_str());
+			loadedOkay = false;
+		}
+		else
+		{
+			printf("loaded urdf string okay!\n");
+			loadedOkay = true;
+		}
+	}
+
+	std::string name;
+
+	bool loadedOkay;
+
+	urdf::Model urdf;
 
 	// collision model
 };
@@ -35,7 +55,7 @@ class ModuleManager
 {
 public:
 
-	ModuleManager();
+	ModuleManager() {};
 
 	ModuleInstance* getRootModule();
 
@@ -51,11 +71,113 @@ public:
 
 	void reconnectReal(/* representation */);
 
+	bool ensureModuleTypeIsLoaded(std::string type);
+
 	std::vector<ModuleInstance> modules;
+
+	std::map<std::string, ModuleType> loadedModuleTypes;
 };
+
+bool ModuleManager::ensureModuleTypeIsLoaded(std::string type)
+{
+	// if this module is already loaded then it's okay
+	std::map<std::string, ModuleType>::iterator modType = loadedModuleTypes.find(type);
+
+	if (modType != loadedModuleTypes.end())
+		return true;
+
+	// if not try and load the module
+	ros::NodeHandle nh("~");
+	std::string moduleParamName = "module_description_" + type;
+	std::string moduleURDFString;
+	if(!ros::param::get(moduleParamName, moduleURDFString))
+	{
+		ROS_ERROR("Error loading module type \"%s\" URDF parameter \"%s\" not present.", type.c_str(), moduleParamName.c_str());
+		return false;
+	}
+
+	ModuleType newModuleType(type, moduleURDFString);
+
+	if (newModuleType.loadedOkay)
+		loadedModuleTypes.emplace(type, newModuleType);
+
+	return true;
+}
 
 
 int main (int argc, char **argv)
 {
+	ros::init(argc, argv, "module_manager");
+	ros::start();
+
+	ros::NodeHandle nh("~");
+
+	ModuleManager modman;
+
+	XmlRpc::XmlRpcValue moduleYaml;
+
+	if(!ros::param::get("/modules", moduleYaml))
+	{
+		ROS_ERROR("Failed to read module config from param server");
+		exit(1);
+	}
+
+	if (moduleYaml.getType() == XmlRpc::XmlRpcValue::TypeArray)
+	{
+		printf("Modules is an array, that's good I guess.\n");
+
+		printf("size of modules is [%d]\n", moduleYaml.size());
+	}
+
+	for (int i=0; i<moduleYaml.size(); ++i)
+	{
+		if (moduleYaml[i].getType() == XmlRpc::XmlRpcValue::TypeStruct)
+			printf("module [%d] is a struct, good I guess!\n", i);
+
+		std::string id = moduleYaml[i]["id"];
+		std::string type = moduleYaml[i]["type"];
+		bool isRoot = moduleYaml[i]["root"];
+
+		if (id != "" && type != "")
+		{
+			printf("Found a module definition okay.\nid : %s\ntype : %s\n", id.c_str(), type.c_str());
+			if (isRoot)
+				printf("Root module\n");
+			else
+				printf("Not root module\n");
+
+			if (modman.ensureModuleTypeIsLoaded(type))
+				printf("module type is okay.\n");
+			else
+				printf("failed to load module type!\n");
+		}
+
+	}
+
+	//std::string modules;
+	//nh.param("/rosdistro", modules, std::string("not_set"));
+
+	//printf("module setup was\n---------\n%s\n", modules.c_str());
+
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
